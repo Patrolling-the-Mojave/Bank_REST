f5ae6e4df3d2b0fadbd0c6afd53b69f8e48f4e9c
Соловьенко Никита <nikidan6032638@yandex.ru>
2026-03-06 15:21:34 +0300

    init

---
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -0,0 +1,16 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+}
---
+++ src/main/resources/db/migration/002_insert_roles.sql
@@ -0,0 +1,2 @@
+INSERT INTO roles (id, name) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'ROLE_USER');
+INSERT INTO roles (id, name) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'ROLE_ADMIN');
